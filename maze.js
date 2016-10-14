3ea337449b462c17b31e6fdac0a8ed9f540acecd
Camille Beckford <Camille Beckford>
2016-10-14 16:21:11 -0700

    EX2

--- maze.js
+++ maze.js
@@ -1,7 +1,18 @@
-window.onload = function(){
-document.getElementById("boundary1").onmouseenter = function() {mouseEnter()};
-
-function mouseEnter() {
-   document.getElementById("boundary1").className+=' youlose';
-}
+window.onload = function()
+{
+	var bounds = document.getElementsByClassName("boundary");
+	for (var i = 0; i < bounds.length - 1; i++)
+    {
+  		bounds[i].onmouseenter = function() {theseWalls()};
+   	}
+	 
 };
+
+function theseWalls()
+{
+	var bounds = document.querySelectorAll(".boundary");
+	for (var i = 0; i < bounds.length - 1; i++)
+    {
+  		bounds[i].className= 'boundary youlose';
+   	}
+}
